91ab378609ec5fedfb52fc3fca9d3ffba751a8f8
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2022-06-13 15:18:34 -0400

    Initial Commit

---
+++ DevDream/DevDreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevDreamApp.swift
+//  DevDream
+//
+//  Created by Christopher Mattar on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DevDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
